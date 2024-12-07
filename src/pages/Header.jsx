5a6395d74f19a5b4acb297b1941073a10035f1e2
AgamJain2706 <agam2003jain@gmail.com>
2024-12-07 20:26:18 +0530

    error

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,5 +1,5 @@
 
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useLocation } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { Dropdown, Ripple, initTWE, } from "tw-elements";
 import axios from "axios";
@@ -24,10 +24,6 @@ function Header({ sidebarToggle, setSidebarToggle }) {
         };
     }, [lastScrollY]);
 
-
-
-
-
     const [logo, setlogo] = useState([])
     const getlogo = async () => {
         try {
@@ -44,13 +40,19 @@ function Header({ sidebarToggle, setSidebarToggle }) {
     useEffect(() => {
         getlogo()
     }, [])
+    const location = useLocation();
+    useEffect(() => {
+        setSidebarToggle(false);
+
+    }, [location])
+
 
     initTWE({ Dropdown, Ripple });
     return (
         <>
             <nav className="bg-[#1A1B33] z-50 p-1 fixed w-full md:ml-0" >
                 <div className="  mx-auto sm:px-4 relative">
-                    <FaBars className="right-2 absolute  top-1.5 ml-2 size-5 sm:size-8 md:hidden " onClick={() => setSidebarToggle(!sidebarToggle)} />
+                    <FaBars className="right-2 absolute  top-1.5 ml-2 size-5 sm:size-8 md:hidden " onClick={() => setSidebarToggle (!sidebarToggle)} />
                     <div className="grid grid-cols-[50%_auto] md:grid-cols-[30%_auto] lg:grid-cols-[50%_auto] items-center  md:h-16 h-9 sm:h-12">
                         <NavLink to="/" >
                             {
@@ -61,8 +63,7 @@ function Header({ sidebarToggle, setSidebarToggle }) {
 
                         <div className="hidden md:block  ">
                             <div className="  flex items-baseline  place-content-evenly ">
-
-                                <NavLink to="/" className="active:text-[#FCB71B] focus:text-[#FCB71B] active:bg-gray-700 focus:bg-gray-700  text-gray-300 hover:bg-gray-700 hover:text-[#FCB71B] px-3 py-2 rounded-md text-sm font-medium ">Home</NavLink>
+                                <NavLink to="/" className="active:text-[#FCB71B] focus:text-[#FCB71B] active:bg-gray-700 focus:bg-gray-700  text-gray-300 hover:bg-gray-700 hover:text-[#FCB71B] px-3 py-4 rounded-md text-sm font-medium ">Home</NavLink>
 
                                 <NavLink to="/about" className="active:text-[#FCB71B] focus:text-[#FCB71B] active:bg-gray-700 focus:bg-gray-700  text-gray-300 hover:bg-gray-700 hover:text-[#FCB71B] px-3 py-2 rounded-md text-sm font-medium ">About Us</NavLink>
 
